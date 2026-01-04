7e2b9959a53126857af7acf336ab4c8e95d8f855
i2513745 <i2513745@continental.edu.pe>
2026-01-04 00:51:38 -0500

    CAPA DATOS
    Crear los métodos  de insertar, actualizar  y eliminar

--- capaDatos/dProducto.py
+++ capaDatos/dProducto.py
@@ -11,4 +11,31 @@ class DProducto:
             listaProductos = self.__db.table(self.__tabla).select('*').execute()
             return listaProductos.data
         except Exception as e:
-            return f'Error: '
+            return f"Error: {e}"
+        
+    def insertarProducto(self, data):
+        try:
+            self.__db.table(self.__tabla).insert(data).execute()
+            return True
+        except Exception as e:
+            return f"Error: {e}"
+
+    def actualizarProducto(self, id_producto, data):
+        try:
+            self.__db.table(self.__tabla)\
+                .update(data)\
+                .eq("id", id_producto)\
+                .execute()
+            return True
+        except Exception as e:
+            return f"Error: {e}"
+
+    def eliminarProducto(self, id_producto):
+        try:
+            self.__db.table(self.__tabla)\
+                .delete()\
+                .eq("id", id_producto)\
+                .execute()
+            return True
+        except Exception as e:
+            return f"Error: {e}"
