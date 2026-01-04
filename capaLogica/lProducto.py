817b2bd22d50624256a2b1d296ee5d13cd5da475
i2513745 <i2513745@continental.edu.pe>
2026-01-04 00:52:17 -0500

    CAPA LOGICA
    Crear metodo crear producto, editar y eliminar

--- capaLogica/lProducto.py
+++ capaLogica/lProducto.py
@@ -6,3 +6,14 @@ class LProducto:
 
     def listarProductos(self):
         return self.__dProducto.obtenerProductos()
+    
+    def crearProducto(self, data):
+        if not data["nombre"] or data["precio"] <= 0 or data["stock"] < 0:
+            raise ValueError("Datos inválidos")
+        return self.__dProducto.insertarProducto(data)
+
+    def editarProducto(self, id_producto, data):
+        return self.__dProducto.actualizarProducto(id_producto, data)
+
+    def eliminarProducto(self, id_producto):
+        return self.__dProducto.eliminarProducto(id_producto)
