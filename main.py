c62f60587701788bd1ce4d8e06a4fbaea2524a80
i2513751 <i2513751@continental.edu.pe>
2026-01-05 19:01:15 -0500

    Main
    Main completado

---
+++ main.py
@@ -0,0 +1,37 @@
+import streamlit as st
+
+# Inicialización de sesión
+if "login" not in st.session_state:
+    st.session_state["login"] = False
+
+if "rol" not in st.session_state:
+    st.session_state["rol"] = ""
+
+if "id_usuario" not in st.session_state:
+    st.session_state["id_usuario"] = 0
+
+from capaPresentacion.pLogin import PLogin
+from capaPresentacion.pProducto import PProducto
+from capaPresentacion.pVenta import PVenta
+
+# LOGIN
+if not st.session_state["login"]:
+    PLogin().mostrar()
+    st.stop()
+
+# DEBUG (puedes quitarlo luego)
+st.write("ROL ACTUAL:", st.session_state["rol"])
+
+st.title("Sistema de Ventas")
+st.success("Bienvenido")
+
+# VENTAS → SOLO VENDEDOR
+if st.session_state["rol"] == "VENDEDOR":
+    PVenta().mostrar()
+    PProducto().mostrar()
+
+
+# ADMIN (opcional, info extra)
+if st.session_state["rol"] == "ADMIN":
+    PProducto().mostrar()
+    st.info("Panel de administración")
